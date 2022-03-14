7f7aafe0113c51e905d44bcf94477c4eaa8647c0
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-03-13 22:59:57 -0300

    Movies Section Creation

---
+++ src/Components/Movies.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Movies.module.css'
+
+const Movies = () => {
+  return (
+    <div className={styles.movies}>Movies</div>
+  )
+}
+
+export default Movies
