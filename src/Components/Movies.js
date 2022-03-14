490e00ba85700e7f534a8c551355e54d14e478db
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-03-14 19:15:51 -0300

    Modal Logic

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -7,6 +7,8 @@ Modal.setAppElement('#root')
 
 const Movies = () => {
 
+
+
 const moviesList = [
       {id: 1, title: "Harry Potter and the Philosopher's Stone", date:'16 November, 2001', image: 'https://fbwebsitedefaultstorage.blob.core.windows.net/prod-film/4835/uk/harry-potter-and-the-philosophers-stone-special-approval-required-1-poster.jpg', sinopse: "Harry Potter lives with his abusive aunt and uncle, Vernon and Petunia Dursley and their bullying son, Dudley. On Harry's eleventh birthday, a half-giant named Rubeus Hagrid personally delivers an acceptance letter to Hogwarts School of Witchcraft and Wizardry, revealing that Harry's parents, James and Lily Potter, were wizards. When Harry was one year old, an evil and powerful dark wizard, Lord Voldemort, murdered his parents. Harry survived Voldemort's killing curse that rebounded off his forehead and seemingly destroyed the Dark Lord, leaving a lightning bolt-shaped scar on his forehead. Unknown to Harry, he is famous in the wizarding world."},
       {id: 2, title: "Harry Potter and the Chamber of Secrets", date:'15 November, 2002', image: 'https://i.pinimg.com/550x/48/7b/a5/487ba5f1de4236f85b70cb0fa97c0f4d.jpg', sinopse: "Harry Potter lives with his abusive aunt and uncle, Vernon and Petunia Dursley and their bullying son, Dudley. On Harry's eleventh birthday, a half-giant named Rubeus Hagrid personally delivers an acceptance letter to Hogwarts School of Witchcraft and Wizardry, revealing that Harry's parents, James and Lily Potter, were wizards. When Harry was one year old, an evil and powerful dark wizard, Lord Voldemort, murdered his parents. Harry survived Voldemort's killing curse that rebounded off his forehead and seemingly destroyed the Dark Lord, leaving a lightning bolt-shaped scar on his forehead. Unknown to Harry, he is famous in the wizarding world."},
@@ -17,12 +19,15 @@ const moviesList = [
       {id: 7, title: 'item #1', date:'16 November, 2001', image: 'https://fbwebsitedefaultstorage.blob.core.windows.net/prod-film/4835/uk/harry-potter-and-the-philosophers-stone-special-approval-required-1-poster.jpg', sinopse: "Harry Potter lives with his abusive aunt and uncle, Vernon and Petunia Dursley and their bullying son, Dudley. On Harry's eleventh birthday, a half-giant named Rubeus Hagrid personally delivers an acceptance letter to Hogwarts School of Witchcraft and Wizardry, revealing that Harry's parents, James and Lily Potter, were wizards. When Harry was one year old, an evil and powerful dark wizard, Lord Voldemort, murdered his parents. Harry survived Voldemort's killing curse that rebounded off his forehead and seemingly destroyed the Dark Lord, leaving a lightning bolt-shaped scar on his forehead. Unknown to Harry, he is famous in the wizarding world."},
       {id: 8, title: 'item #1', date:'16 November, 2001', image: 'https://fbwebsitedefaultstorage.blob.core.windows.net/prod-film/4835/uk/harry-potter-and-the-philosophers-stone-special-approval-required-1-poster.jpg', sinopse: "Harry Potter lives with his abusive aunt and uncle, Vernon and Petunia Dursley and their bullying son, Dudley. On Harry's eleventh birthday, a half-giant named Rubeus Hagrid personally delivers an acceptance letter to Hogwarts School of Witchcraft and Wizardry, revealing that Harry's parents, James and Lily Potter, were wizards. When Harry was one year old, an evil and powerful dark wizard, Lord Voldemort, murdered his parents. Harry survived Voldemort's killing curse that rebounded off his forehead and seemingly destroyed the Dark Lord, leaving a lightning bolt-shaped scar on his forehead. Unknown to Harry, he is famous in the wizarding world."},
   ]
+  const initialState = moviesList[0]; 
+
   const [modailIsOpen, setModailIsOpen] = React.useState(false);
   const [movies, setMovies] = React.useState(moviesList);
+  const [selectMovie, setSelectedMovie] = React.useState(initialState);
   
 
-  const handleSelectedMovie = (id) => {
-    setModailIsOpen(movies.find(movie => movie.id === id))
+  const handleSelectedMovie = (post) => {
+    setSelectedMovie(post);
     setModailIsOpen(true);
   }
 
@@ -38,7 +43,7 @@ const moviesList = [
       <div className={styles.carousel}>
            {moviesList.map((post, key) => {
             return (
-            <div className={styles.card} onClick={handleSelectedMovie} key={key} 
+            <div className={styles.card} onClick={() => handleSelectedMovie(post)} key={key} 
             style={{backgroundImage: `url('${post.image}')`,
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
@@ -57,21 +62,16 @@ const moviesList = [
       isOpen={modailIsOpen}
       onRequestClose={handleCloseModal}
       className={styles.modal}>
-        {moviesList.filter(function (selecMovie) {
-        return selecMovie.id === 1;
-    }).map((post, key) => {
-          return (
-            <div className={styles.content} key={key} onClick={handleCloseModal}>
-              <div className={styles.imageModal} style={{backgroundImage: `url('${post.image}')`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'left',
-            backgroundSize: 'contain'}}/>
-              <h1 className={styles.titleModal}>{post.title}</h1>
-              <p className={styles.dateModal}>{post.date}</p>
-              <p className={styles.sinopsisModal}>{post.sinopse}</p>
+         <div className={styles.content} onClick={handleCloseModal}>
+              <div className={styles.imageModal} style={{backgroundImage: `url('${selectMovie.image}')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'left',
+              backgroundSize: 'contain'}}/>
+              <h1 className={styles.titleModal}>{selectMovie.title}</h1>
+              <p className={styles.dateModal}>{selectMovie.date}</p>
+              <p className={styles.sinopsisModal}>{selectMovie.sinopse}</p>
             </div>
-          )
-        })}
+        
         
       </Modal>
       </div>   
